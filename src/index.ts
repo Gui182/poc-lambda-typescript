8b3948293544bb54a7e88ec2c1c0f6508cf25dea
Gui182 <guilherme.oliveira182@yahoo.com.br>
2024-08-05 14:58:18 -0300

    feat: url params

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,14 @@ export const handler = async (
 
   const queryParams = event.queryStringParameters || {};
   const channelId = queryParams.channelId
+  const clientId = event.pathParameters?.clientId;
+  const skuId = event.pathParameters?.skuId;
   const ramdomNumber = Math.random()
 
   const response = {
     statusCode: 200,
     body: JSON.stringify({
-      message: `O numero aleátorio gerado é ${ramdomNumber} e o queryParams é ${channelId}`,
+      message: `O numero aleátorio gerado é ${ramdomNumber}, o queryParams é ${channelId}, o clientId é ${clientId}, o skuId é ${skuId}`,
       input: event,
     }),
   };
